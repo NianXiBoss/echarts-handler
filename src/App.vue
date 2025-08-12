81947dd86c3fc2d096031e01238a543f01b76c9a
WangJialong <282373762@qq.com>
2025-08-12 23:49:26 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="common-layout w-full h-full">
+    <el-container class="w-full h-full">
+      <!--   顶部导航栏   -->
+      <el-header>Header</el-header>
+      <el-container>
+        <!--   侧边栏   -->
+        <el-aside width="200px">Aside</el-aside>
+        <!--    主区域    -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style scoped></style>
