ab42ccd16333dc7be551ca05ae98293fbe3d67f8
WangJialong <282373762@qq.com>
2025-08-15 00:38:39 +0800

    样式优化

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
   <div class="common-layout w-full h-full">
     <el-container class="w-full h-full">
       <!--   顶部导航栏   -->
-      <el-header>Header</el-header>
+      <el-header class="h-[52px]! bg-[#fff] shadow-sm relative z-[1] px-[24px]! py-[8px]!">
+        导航栏
+      </el-header>
       <el-container>
         <!--   侧边栏   -->
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px" class="bg-[#fff] shadow-sm"> 侧边栏 </el-aside>
         <!--    主区域    -->
-        <el-main>Main</el-main>
+        <el-main> 主区域 </el-main>
       </el-container>
     </el-container>
   </div>
