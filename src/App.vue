3c4d81e59ea5119f8e2b6b248a748df44fc48d21
WangJialong <282373762@qq.com>
2025-08-17 15:28:25 +0800

    顶部导航栏(初版)及图标配置文件修改

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { CopyDocument, Refresh } from '@element-plus/icons-vue'
+</script>
 
 <template>
   <div class="common-layout w-full h-full">
     <el-container class="w-full h-full">
       <!--   顶部导航栏   -->
-      <el-header class="h-[52px]! bg-[#fff] shadow-sm relative z-[1] px-[24px]! py-[8px]!">
-        导航栏
+      <el-header
+        class="h-[52px]! bg-[#fff] shadow-sm relative z-[1] px-[24px]! py-[8px]! flex items-center justify-between"
+      >
+        <div class="flex items-center">
+          <img src="@/assets/logo.png" alt="系统图标" class="w-8" />
+          <h3 class="ml-3! font-bold text-[20px] text-[#1E293B]">ECharts-可视化配置平台</h3>
+        </div>
+        <div>
+          <el-button :icon="Refresh">重置</el-button>
+          <el-button :icon="CopyDocument" type="primary">复制代码</el-button>
+        </div>
       </el-header>
       <el-container>
         <!--   侧边栏   -->
