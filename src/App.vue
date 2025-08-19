06096cb4ecf8dc93f677b007cc9021eb065a6ea5
WangJialong <282373762@qq.com>
2025-08-20 00:08:33 +0800

    侧边导航栏是否展开

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
-import { CopyDocument, Fold, Refresh } from '@element-plus/icons-vue'
+import { CopyDocument, Expand, Fold, Menu, Refresh } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+
+/**
+ * 侧边栏是否展开
+ */
+const title = ref<string>('收起')
+const isUnfold = ref<boolean>(true)
+const toggleFold = () => {
+  isUnfold.value = !isUnfold.value
+  title.value = isUnfold.value ? '收起' : '展开'
+}
 </script>
 
 <template>
@@ -20,15 +31,33 @@ import { CopyDocument, Fold, Refresh } from '@element-plus/icons-vue'
       </el-header>
       <el-container>
         <!--   侧边栏   -->
-        <el-aside class="bg-[#fff] shadow-sm w-50! px-6 py-4 flex flex-col">
-          <h4
-            class="text-[18px] font-bold text-[#1E293B] text-center after:w-full after:h-[1px] after:bg-[#e5e7eb] after:block after:mt-2!"
+        <el-aside
+          class="bg-[#fff] shadow-sm px-6 py-4 flex flex-col transition-all duration-300 ease-in-out"
+          :class="isUnfold ? 'w-50!' : 'w-25!'"
+        >
+          <div
+            class="flex items-center justify-center relative w-full h-10 text-[18px] font-bold text-[#1E293B] after:w-full after:h-[1px] after:bg-[#e5e7eb] after:absolute after:mt-3! after:bottom-0"
           >
-            图表类型
-          </h4>
+            <el-icon class="mr-1!"><Menu class="text-[#409eff]" /></el-icon>
+            <h4
+              class="transition-all duration-350 ease-out overflow-hidden whitespace-nowrap"
+              :class="
+                isUnfold
+                  ? 'opacity-[1] translate-x-[0px] w-auto'
+                  : 'opacity-[0] translate-x-[10px] w-0'
+              "
+            >
+              图表类型
+            </h4>
+          </div>
           <ul class="w-full flex-1"></ul>
-          <div class="w-full h-10 flex items-center justify-center relative before:w-full before:h-[1px] before:bg-[#e5e7eb] before:absolute before:top-0">
-            <el-icon class="text-[20px]! cursor-pointer"><Fold class="hover:text-[#409eff]"/></el-icon>
+          <div
+            class="w-full h-10 flex items-center justify-center relative before:w-full before:h-[1px] before:bg-[#e5e7eb] before:absolute before:top-0"
+          >
+            <el-icon class="text-[20px]! cursor-pointer" :title @click="toggleFold">
+              <Fold class="hover:text-[#409eff]" v-if="isUnfold" />
+              <Expand class="hover:text-[#409eff]" v-else />
+            </el-icon>
           </div>
         </el-aside>
         <!--    主区域    -->
